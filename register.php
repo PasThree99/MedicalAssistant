84ab9be35bd50f6b055a8e84da2b99516f296605
sebgogon <sebastian.y.gonzalez@oracle.com>
2022-07-20 19:23:45 -0500

    insert try

--- register.php
+++ register.php
@@ -11,7 +11,16 @@
 
 
     if(isset($_GET['submit'])){
-        echo "yep";
+        $curp = $_GET['id'];
+        $name = $_GET['name'];
+        $ln1 = $_GET['ln1'];
+        $ln2 = $_GET['ln2'];
+        $date = $_GET['date'];
+        $hg = $_GET['height'];
+
+        $sql = 'insert into pacientes (curp,nombre,apellido_paterno,apellido_materno,fecha_de_nacimiento,estatura) values ("'.$curp . '","' . $name . '","' . $ln1 . '","' . $ln2 . '","' . $date . '","' . $hg .'")';
+        $con->query($sql);
+        
     }
 ?>
 
@@ -29,11 +38,11 @@
         Register a patient
         <form action="" method="get">
             <input placeholder="CURP" name="id">
-            <input placeholder="Name" name="id">
-            <input placeholder="Last name 1" name="id">
-            <input placeholder="Last name 2" name="id">
+            <input placeholder="Name" name="name">
+            <input placeholder="Last name 1" name="ln1">
+            <input placeholder="Last name 2" name="ln2">
             <input placeholder="" name="date" type="date">
-            <input placeholder="Heigth" name="id" type="number" min=".30" max="2.2" value="1.60" step="0.01">
+            <input placeholder="Height" name="height" type="number" min=".30" max="2.2" value="1.60" step="0.01">
             <input type="submit" value="Search" name="submit" >
         </form>
        
