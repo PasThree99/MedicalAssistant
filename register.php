05c74d5c7e640fa6562c6c23724fa4bfb70c3e05
sebgogon <sebastian.y.gonzalez@oracle.com>
2022-07-20 19:29:34 -0500

    insert correction 3

--- register.php
+++ register.php
@@ -18,7 +18,8 @@
         $date = $_GET['date'];
         $hg = $_GET['height'];
 
-        $sql = 'insert into pacientes (curp,nombre,apellido_paterno,apellido_materno,fecha_de_nacimiento,estatura) values ("'.$curp . '","' . $name . '","' . $ln1 . '","' . $ln2 . '","' . $date . '","' . $hg .'")';
+        $sql = "insert into pacientes (curp,nombre,apellido_paterno,apellido_materno,fecha_de_nacimiento,estatura) values ('$curp ',' $name ',' $ln1 ',' $ln2  ','  $date  ',' $hg ')";
+        
         $con->query($sql);
         
     }
